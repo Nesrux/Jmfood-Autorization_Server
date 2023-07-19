f99b77e3a99453d52febd2919152442fdab0be5e
Nesrux <joaomarcosdevs@gmail.com>
2023-07-19 19:46:45 -0300

    Config: configurando o fluxo do Authorization Server

--- src/main/java/com/nesrux/jmfood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/nesrux/jmfood/auth/AuthorizationServerConfig.java
@@ -1,14 +1,41 @@
 package com.nesrux.jmfood.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
-
 	
+	@Autowired
+	private PasswordEncoder pass;
+	@Autowired
+	AuthenticationManager menager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		clients.inMemory()
+			.withClient("jmfoodWeb")
+				.secret(pass.encode("web123"))
+				.authorizedGrantTypes("password")
+				.scopes("write", "read")
+			.and()
+				.withClient("jmfoodMobile")
+				.secret(pass.encode("mobile123"))
+				.authorizedGrantTypes("password")
+				.scopes("write", "read");
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(menager);
+	}
 
 }
