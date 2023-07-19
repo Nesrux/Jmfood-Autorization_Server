eb7f4bc9825e260938627bee39c21cb6be7f6c6c
Nesrux <joaomarcosdevs@gmail.com>
2023-07-19 19:01:09 -0300

    :tada: primeiro commit

---
+++ src/main/java/com/nesrux/jmfood/auth/JmfoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nesrux.jmfood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmfoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmfoodAuthApplication.class, args);
+	}
+
+}
