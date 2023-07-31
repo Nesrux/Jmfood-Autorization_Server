59e0fde2c1c9e80d3287b113caaa8cc7fac798c7
Nesrux <joaomarcosdevs@gmail.com>
2023-07-31 14:51:20 -0300

    Feat: adicionando implementação para autenticar usuario do banco de dados

---
+++ src/main/java/com/nesrux/jmfood/auth/core/AuthUser.java
@@ -0,0 +1,24 @@
+package com.nesrux.jmfood.auth.core;
+
+import java.util.Collections;
+
+import org.springframework.security.core.userdetails.User;
+
+import com.nesrux.jmfood.auth.domain.model.Usuario;
+
+import lombok.Getter;
+
+@Getter
+public class AuthUser extends User {
+
+	private String nome;
+	private String email;
+
+	private static final long serialVersionUID = 1L;
+
+	public AuthUser(Usuario usuario) {
+		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+		this.nome = usuario.getNome();
+	}
+
+}
--- src/main/java/com/nesrux/jmfood/auth/core/JpaUserDetailsService.java
+++ src/main/java/com/nesrux/jmfood/auth/core/JpaUserDetailsService.java
@@ -5,13 +5,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.nesrux.jmfood.auth.domain.model.Usuario;
+import com.nesrux.jmfood.auth.domain.repository.UsuarioRepository;
+
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
 
+	private UsuarioRepository repository;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario usuario = repository.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Usuario com o Email informado não foi encontrado") );
+
+		return new AuthUser(usuario);
 	}
 
 }
