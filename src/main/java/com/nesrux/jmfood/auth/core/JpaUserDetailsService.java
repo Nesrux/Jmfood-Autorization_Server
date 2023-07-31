0ca95f2f5777ff7e15489fb53e2d4ca907b8d1c5
Nesrux <joaomarcosdevs@gmail.com>
2023-07-31 14:43:26 -0300

    Feat: adicionando uma clase de service para o user, (userDetailService

---
+++ src/main/java/com/nesrux/jmfood/auth/core/JpaUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.nesrux.jmfood.auth.core;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
