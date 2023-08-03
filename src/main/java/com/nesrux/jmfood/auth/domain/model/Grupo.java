dd6c316c63f7590ab96830a898e5dace5580520c
Nesrux <joaomarcosdevs@gmail.com>
2023-08-03 16:48:16 -0300

    Feat: Adicionandoas Entidades de grupos e permissões ao authozization server

---
+++ src/main/java/com/nesrux/jmfood/auth/domain/model/Grupo.java
@@ -0,0 +1,41 @@
+package com.nesrux.jmfood.auth.domain.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class Grupo {
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"), inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private Set<Permissao> permissoes = new HashSet<>();
+
+	public void associarPermissao(Permissao permissao) {
+		permissoes.add(permissao);
+	}
+
+	public void deassociarPermissao(Permissao permissao) {
+		permissoes.remove(permissao);
+	}
+}
